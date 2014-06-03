870a59ebb2795d0085ac440f8910529e7319fbeb
Erin Carter <erin.s.carter@gmail.com>
2014-06-03 11:37:46 -0500

    Basics exercises

---
+++ Basics_ex3.rb
@@ -0,0 +1,5 @@
+movies = { Alien:1975, BladeRunner:1982, ET:1986, Terminator:1984}
+puts movies[:Alien]
+puts movies[:BladeRunner]
+puts movies[:ET]
+puts movies[:Terminator]
---
+++ Basics_ex4.rb
@@ -0,0 +1,5 @@
+dates = [1975, 1982, 1986, 1984]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
